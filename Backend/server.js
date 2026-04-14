af8a2fb8b23c1e367431be1b619982a416ca5acc
Sai Varshini <saivarshinik123@email.com>
2026-04-14 19:51:58 +0530

    updated server code for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,17 @@
-require('dotenv').config({ path: __dirname + '/.env' });
+require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 
 app.use(cors({
-  origin: "https://monumental-centaur-fd1d96.netlify.app"
+  origin: "https://monumental-centaur-fd1d96.netlify.app",
+  credentials: true
 }));
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../Frontend')));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
@@ -19,15 +19,12 @@ app.use('/api/transactions', require('./routes/transactions'));
 app.use('/api/password', require('./routes/password'));
 app.use('/api/admin', require('./routes/admin'));
 
-app.get('/{*splat}', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/index.html'));
-});
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected ✅');
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT || 5000} ✅`);
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT} ✅`);
     });
   })
   .catch(err => {
